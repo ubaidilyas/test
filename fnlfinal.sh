cc839a5be3220e03e7917bb07e6ad5be3c77b1eb
Ubaid Ilyas <ubaid.ilyas@mondia.com>
2020-03-18 16:20:45 +0100

    "testing"

--- fnlfinal.sh
+++ fnlfinal.sh
@@ -19,7 +19,7 @@ fi
 rm $id.*
 done <test_unique_nodes.txt
 rm 2id.txt allocations.txt test_together.txt jobid.txt id.txt nodes.* 
-echo "Application:Count:CPU:Memory" > current.xls
+echo "Application:Count:CPU:Memory" > current.txt
 while IFS=: read -r jobid id; do
 curl --header "X-Nomad-Token: 4d67205e-b898-00c6-63ce-6ee324da5a74" http://172.21.38.9:4646/v1/allocation/$id > $jobid.json
 tr , '\n' < $jobid.json > $jobid.txt
@@ -28,23 +28,25 @@ if [ -z $string ]; then
 	memory=`grep '"MemoryMB' $jobid.txt | grep -o '"MemoryMB.*'| cut -d':' -f2 | tr '\n' ' ' | perl -MList::Util=max -lane 'print max(@F)'`
 	count=`grep 'Count' $jobid.txt |tr ':' ' ' |perl -MList::Util=max -lane 'print max(@F)'`
 	cpu=`grep '"CPU' $jobid.txt | grep -o '"CPU.*'| cut -d':' -f2 | tr '\n' ' ' | perl -MList::Util=max -lane 'print max(@F)'`
-	echo "$jobid:$count:$cpu:$memory" >> current.xls
+	echo "$jobid:$count:$cpu:$memory" >> current.txt
 else
 		memory=`grep '"MemoryMB' $jobid.txt | grep -o '"MemoryMB.*'| cut -d':' -f2 | tr '\n' ' ' | perl -MList::Util=max -lane 'print max(@F)'`
 		count=`grep 'Count' $jobid.txt |tr ':' ' ' |perl -MList::Util=max -lane 'print max(@F)'`
 		cpu=`grep '"CPU' $jobid.txt | grep -o '"CPU.*'| cut -d':' -f2 | tr '\n' ' ' | perl -MList::Util=max -lane 'print max(@F)'`
 stringvar1=`grep "Constraints" $jobid.txt | grep "meta.dc"`
 if [ -z $stringvar1 ]; then
-echo "$jobid:$((ccount*count)):$cpu:$memory" >> current.xls
+echo "$jobid:$((ccount*count)):$cpu:$memory" >> current.txt
 else
-echo "$jobid:$((azwe*count)):$cpu:$memory" >> current.xls
+echo "$jobid:$((azwe*count)):$cpu:$memory" >> current.txt
 fi
 fi
 rm $jobid.txt $jobid.json
 done <test_unique_together.txt
 
-cat current.xls>>`date +%m`.xls
-sort -t: -k1,1 previous.xls | uniq -c | cut -d" " -f4 > days.xls
-sort -t: -k1,1 previous.xls | uniq -c | cut -d" " -f5 > app.xls
-paste -d: app.xls days.xls > `date +%d-%m-%Y`.xls
+cat current.txt>>`date +%m`.txt
+sort -t: -k1,1 previous.txt | uniq -c | cut -d" " -f4 > days.txt
+sort -t: -k1,1 previous.txt | uniq -c | cut -d" " -f5 > app.txt
+paste -d: app.txt days.txt > `date +%d-%m-%Y`.txt
 
+
+rm test* days* current* app*	
