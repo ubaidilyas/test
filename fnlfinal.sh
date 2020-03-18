991a2b4fa8ff5b56c3ac8a4bb7d41171b6d9f1a4
Ubaid Ilyas <ubaid.ilyas@mondia.com>
2020-03-18 09:00:43 +0100

    kfj

--- fnlfinal.sh
+++ fnlfinal.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-date=`date +%d-%m-%Y`
+date=`date`
 curl --header "X-Nomad-Token: 4d67205e-b898-00c6-63ce-6ee324da5a74" http://172.21.38.9:4646/v1/allocations > allocations.txt
 tr , '\n' < allocations.txt > 2id.txt   
 grep '"JobID' 2id.txt | cut -d\" -f4 > jobid.txt 
