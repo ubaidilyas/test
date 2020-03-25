38b6b99ee58b526a8a4dc59693d1355c64d32629
Ubaid Ilyas <ubaid.ilyas@mondia.com>
2020-03-25 14:46:00 +0100

    "testing"

--- staging_script.sh
+++ staging_script.sh
@@ -44,9 +44,9 @@ rm $jobid.txt $jobid.json
 done <test_unique_together.txt
 
 cat current.txt>>`date +%m`.txt
-sort -t: -k1,1 `date +%m`.txt |cut -d" " -f4 > days.txt
-sort -t: -k1,1 `date +%m`.txt | cut -d" " -f5 > app.txt
+sort -t: -k1,1 `date +%m`.txt | uniq -c | cut -d" " -f4 > days.txt
+sort -t: -k1,1 `date +%m`.txt | uniq -c | cut -d" " -f5 > app.txt
 paste -d: app.txt days.txt > `date +%d-%m-%Y`.txt
 
 
-rm test* current*	
+rm test* days* current* app*	
