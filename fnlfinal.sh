d7fbe866985ad4c563701d3e5ac13535cb430ee5
Ubaid Ilyas <ubaid.ilyas@mondia.com>
2020-04-06 05:55:36 +0200

    improving code

--- fnlfinal.sh
+++ fnlfinal.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 # Creating list of JobID and ID of Apps
-curl --header "X-Nomad-Token: 4d67205e-b898-00c6-63ce-6ee324da5a74" http://172.21.38.9:4646/v1/allocations > allocations.txt
-tr , '\n' < allocations.txt > 2id.txt   
-grep '"JobID' 2id.txt | cut -d\" -f4 > jobid.txt 
-grep '"ID' 2id.txt  | cut -d\" -f4 > id.txt
+curl --header "X-Nomad-Token: 4d67205e-b898-00c6-63ce-6ee324da5a74" http://172.21.38.9:4646/v1/allocations > test_allocations.txt
+tr , '\n' < test_allocations.txt > test_2id.txt   
+grep '"JobID' test_2id.txt | cut -d\" -f4 > jobid.txt 
+grep '"ID' test_2id.txt  | cut -d\" -f4 > id.txt
 paste -d: jobid.txt id.txt > test_together.txt
 sort -u -t: -k1,1 test_together.txt > test_unique_together.txt
 #Creating list of Clients (for calculating count for type system)
-curl --header "X-Nomad-Token: 4d67205e-b898-00c6-63ce-6ee324da5a74" http://172.21.38.9:4646/v1/nodes >nodes.json
-tr , '\n' <nodes.json > nodes.txt
-ccount=`grep '"ID"' nodes.txt | wc -l`
-grep '"ID"' nodes.txt | cut -d\" -f4 > test_unique_nodes.txt
+curl --header "X-Nomad-Token: 4d67205e-b898-00c6-63ce-6ee324da5a74" http://172.21.38.9:4646/v1/nodes >test_nodes.json
+tr , '\n' <test_nodes.json > test_nodes.txt
+ccount=`grep '"ID"' test_nodes.txt | wc -l`
+grep '"ID"' test_nodes.txt | cut -d\" -f4 > test_unique_nodes.txt
 while IFS=: read -r id; do
 curl --header "X-Nomad-Token: 4d67205e-b898-00c6-63ce-6ee324da5a74" http://172.21.38.9:4646/v1/node/$id > $id.json
 tr , '\n' <$id.json > $id.txt
@@ -21,11 +21,9 @@ azwe=$((ccount-1))
 fi
 rm $id.*
 done <test_unique_nodes.txt
-#Removing unrequired files
-rm 2id.txt allocations.txt test_together.txt jobid.txt id.txt nodes.* 
 
 #Creating initial file for daily report
-echo "Days/Application:Count:CPU:Memory:Size" > `date +%m-%Y`.txt
+echo "Days/Application:Count:CPU:Memory:Size" > test_current.txt
 
 #Calculating cpu,memory,count and size
 while IFS=: read -r jobid id; do
@@ -44,21 +42,20 @@ tr , '\n' < $jobid.json > $jobid.txt
 #Assigning count according to type of application	
 string=`grep '"Type":"system"' $jobid.txt`
 if [ -z $string ]; then
-	echo "$jobid:$count:$cpu:$size" >> `date +%m-%Y`.txt
+	echo "$jobid:$count:$cpu:$size" >> 
 else
 stringvar1=`grep "Constraints" $jobid.txt | grep "meta.dc"`
 if [ -z $stringvar1 ]; then
-echo "$jobid:$((ccount*count)):$cpu:$size" >> `date +%m-%Y`.txt
+echo "$jobid:$((ccount*count)):$cpu:$size" >> test_current.txt
 else
-echo "$jobid:$((azwe*count)):$cpu:$size" >> `date +%m-%Y`.txt
+echo "$jobid:$((azwe*count)):$cpu:$size" >> test_current.txt
 fi
 fi
-
 rm $jobid.txt $jobid.json
 done <test_unique_together.txt
 
 #Creating Daily Report report
+cat test_current.txt>>`date +%m`.txt
 sort -t: -k1,1 `date +%m-%Y`.txt | uniq -c > `date +%d-%m-%Y`.txt
 
-
-rm test*
+rm test* jobid.txt 
