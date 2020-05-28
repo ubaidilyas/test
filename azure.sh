9c2ec941b05ea32e6dcd8a51c8af53816df267cf
Ubaid Ilyas <ubaid.ilyas@mondia.com>
2020-05-28 18:36:48 +0200

    correcting syntax

--- azure.sh
+++ azure.sh
@@ -2,12 +2,15 @@
 
 
 az login --service-principal --username $AZURE_CLIENT_ID --password $AZURE_CLIENT_SECRET --tenant $AZURE_TENANT_ID
-string=`az storage directory exists -s cost-reports -n \`date +%m-%Y\` --account-key $secret --account-name mondiaci | grep -o true`
+directory=`date +%m-%Y`
+string=`az storage directory exists -s cost-reports -n $directory --account-key $secret --account-name mondiaci | grep -o true`
 echo $string
 if [ -z $string ]; then
-		az storage directory create -s cost-reports -n \`date +%m-%Y\`
+		az storage directory create -s cost-reports -n $directory --account-key $secret --account-name mondiaci
 		./fnlfinal.sh
 	else 
-		az storage file download -s cost-reports -p \`date +%m-%Y\`/\`date +%m-%Y\`.txt --dest /mnt/resource/workspace/Testing-admin-jobs/cost-reports --account-key $secret --account-name mondiaci
+		az storage file download -s cost-reports -p $directory/$directory.txt --dest /mnt/resource/workspace/Testing-admin-jobs/cost-reports --account-key $secret --account-name mondiaci
 		./fnlfinal.sh
 	fi
+
+
