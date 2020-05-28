1f39a30185b36921f5f997eeb5433a711f83b32d
Ubaid Ilyas <ubaid.ilyas@mondia.com>
2020-05-28 16:49:36 +0200

    adding azure script

--- azure.sh
+++ azure.sh
@@ -1,15 +1,20 @@
+#!/bin/sh
+
+
 az login --service-principal --username $AZURE_CLIENT_ID --password $AZURE_CLIENT_SECRET --tenant $AZURE_TENANT_ID
 az storage file upload --source /mnt/resource/workspace/Testing-admin-jobs/cost-reports/test_ubaid.txt  -s cost-reports/test --account-key $secret --account-name mondiaci
 az storage file download -s cost-reports -p test/test_ubaid.txt --dest /mnt/resource/workspace/Testing-admin-jobs/cost-reports/download --account-key $secret --account-name mondiaci
-az storage file exists -s cost-reports -p test/test_ubaid.txt --account-key $secret --account-name mondiaci | grep -o true
-#if [ $string=true ]; then
-#		echo " file exists"
-#	else 
-#		echo "file does not exist"
-#	fi
-#string=`az storage file exists -s cost-reports -p test/test_ubaid1.txt --account-key $secret --account-name mondiaci | grep -o true`
-#if [ $string=true ]; then
-#		echo " file exists"
-#	else 
-#		echo "file does not exist"
-#	fi
+string=`az storage file exists -s cost-reports -p test/test_ubaid.txt --account-key $secret --account-name mondiaci | grep -o true`
+echo $string
+if [ $string=true ]; then
+		echo " file exists"
+	else 
+		echo "file does not exist"
+	fi
+string=`az storage file exists -s cost-reports -p test/test_ubaid1.txt --account-key $secret --account-name mondiaci | grep -o true`
+echo $string
+if [ $string=true ]; then
+		echo " file exists"
+	else 
+		echo "file does not exist"
+	fi
