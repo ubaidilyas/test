991fab101c98eadb51b5261a444b35d9ce302c9a
Ubaid Ilyas <ubaid.ilyas@mondia.com>
2020-05-28 18:06:47 +0200

    adding new script for division as per months

--- azure.sh
+++ azure.sh
@@ -2,12 +2,12 @@
 
 
 az login --service-principal --username $AZURE_CLIENT_ID --password $AZURE_CLIENT_SECRET --tenant $AZURE_TENANT_ID
-az storage file upload --source /mnt/resource/workspace/Testing-admin-jobs/cost-reports/test_ubaid.txt  -s cost-reports/test --account-key $secret --account-name mondiaci
-az storage file download -s cost-reports -p test/test_ubaid.txt --dest /mnt/resource/workspace/Testing-admin-jobs/cost-reports/download --account-key $secret --account-name mondiaci
-string=`az storage file exists -s cost-reports -p test/test_ubaid.txt --account-key $secret --account-name mondiaci | grep -o true`
+string=`az storage directory exists -s cost-reports -n \`date +%m-%Y\` --account-key $secret --account-name mondiaci | grep -o true`
 echo $string
 if [ -z $string ]; then
-		echo " file does not exists"
+		az storage directory create -s cost-reports -n \`date +%m-%Y\`
+		./fnlfinal.sh
 	else 
-		./ubaid.sh
+		az storage file download -s cost-reports -p \`date +%m-%Y\`/\`date +%m-%Y\`.txt --dest /mnt/resource/workspace/Testing-admin-jobs/cost-reports --account-key $secret --account-name mondiaci
+		./fnlfinal.sh
 	fi
