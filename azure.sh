b1bc10d715a3f8ecec915b5730eeec4456355ae8
Ubaid Ilyas <ubaid.ilyas@mondia.com>
2020-07-24 09:55:47 +0200

    adding files back

---
+++ azure.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+
+az login --service-principal --username $AZURE_CLIENT_ID --password $AZURE_CLIENT_SECRET --tenant $AZURE_TENANT_ID
+directory=`date +%m-%Y`
+string=`az storage directory exists -s cost-reports -n $directory --account-key $secret --account-name mondiaci | grep -o true`
+echo $string
+if [ -z $string ]; then
+		az storage directory create -s cost-reports -n $directory --account-key $secret --account-name mondiaci
+		./fnlfinal.sh
+	else 
+		az storage file download -s cost-reports -p $directory/$directory.txt --dest /mnt/resource/workspace/Testing-admin-jobs/cost-reports --account-key $secret --account-name mondiaci
+		./fnlfinal.sh
+	fi
+
+
