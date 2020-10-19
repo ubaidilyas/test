8bd6905cd2387afc578428a27ad7e5a6be491c68
Ubaid Ilyas <ubaid.ilyas@mondia.com>
2020-10-19 15:04:44 +0200

    adding Dockerfile and centos.sh

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+#!/bin/sh
+FROM docker.io/centos:8
+RUN yum install -y epel-release jq which wget unzip openssl git && yum clean all -y
+RUN wget https://github.com/go-acme/lego/releases/download/v3.8.0/lego_v3.8.0_linux_amd64.tar.gz
+RUN tar xf lego_v3.8.0_linux_amd64.tar.gz
+RUN mv lego /usr/local/bin/lego
+RUN wget https://releases.hashicorp.com/consul/1.8.0/consul_1.8.0_linux_amd64.zip
+RUN unzip consul_1.8.0_linux_amd64.zip 
+RUN mv consul /usr/local/bin/consul
+WORKDIR /usr/local
+COPY ./centos.sh .
+RUN chmod +x ./centos.sh
+ENTRYPOINT ["/bin/sh", "centos.sh"]
+CMD ["consul_address", "consul_token","cf_dns", "cf_zone"]
---
+++ centos.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+export CF_DNS_API_TOKEN="${3}"
+export CF_ZONE_API_TOKEN="${4}"
+consul kv get -token=${2} -http-addr=${1} -keys -separator="" letsencrypt > list.txt
+while IFS=/ read -r key value type; do
+if [ "$type" = 'cert' ]; then
+echo "${value}=`consul kv get -token=${2} -http-addr=${1} letsencrypt/${value}/${type} | openssl x509 -enddate -noout`" >>enddate.txt
+fi
+done <list.txt
+while IFS== read -r domain notafter date; do
+if [ $(((`date -d "${date}" +%s` - `date +%s`) / 86400)) -lt 30 ]; then
+echo ${domain}
+lego --server=https://acme-staging-v02.api.letsencrypt.org/directory -d "$(echo "${domain}" | sed -e 's/_/*/')" --email sysops@mondia.com --key-type rsa4096 --accept-tos --dns cloudflare --dns-timeout 90 --dns.resolvers 8.8.8.8 run
+jq -n --arg kcert letsencrypt/${domain}/cert --arg kkey letsencrypt/${domain}/key --arg vcert $(base64 -i $(pwd)/.lego/certificates/${domain}.crt| tr -d \\n) --arg vkey $(base64 -i $(pwd)/.lego/certificates/${domain}.key| tr -d \\n) '[{"KV": {"Verb":"set","Key":$kcert,"Value":$vcert}},{"KV": {"Verb":"set","Key": $kkey,"Value": $vkey}}]' > ${domain}.json
+      fi
+done <enddate.txt
+if [ -n $(ls *.json|wc -l) ]; then
+jq -s 'add ' *.json >put.json
+cat put.json
+#curl -H "X-Consul-Token: ${2}" --request PUT --data @put.json ${1}/v1/txn
+fi
+rm put.json
+mv *.json $(pwd)/.lego/
+rm *.txt
